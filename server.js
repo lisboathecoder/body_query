acd571b916e11c1ab401e392d1fe7862da92af1a
lisboathegoat <gustavo.lisboa2009@gmail.com>
2025-09-01 08:02:57 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,78 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import dados from "./src/data/dados.js";
+const { bruxos } = dados;
+
+const app = express();
+app.use(express.json());
+
+dotenv.config();
+const serverPort = process.env.PORT || 3001;
+
+app.get("/", (req, res) => {
+    res.send("🚀 Servidor funcionando...");
+});
+
+app.get('/bruxos', (req, res) => {
+    const { casa, ano, especialidade, nome } = req.query;
+    let resultado = bruxos;
+  
+    if (casa) {
+      resultado = resultado.filter(b => b.casa.toLowerCase() === casa.toLowerCase());
+    }
+  
+    if (ano) {
+      resultado = resultado.filter(b => b.ano == ano);
+    }
+  
+    if (especialidade) {
+      resultado = resultado.filter(b => b.especialidade.toLowerCase().includes(especialidade.toLowerCase()));
+    }
+  
+    if (nome) {
+      resultado = resultado.filter(b => b.nome.toLowerCase().includes(nome.toLowerCase()));
+    }
+  
+    res.status(200).json({
+      total: resultado.length,
+      data: resultado
+    });
+});
+
+app.post('/bruxos', (req, res) => {
+    // Acessando dados do body
+    const { nome, casa, ano, varinha, mascote, patrono, especialidade, vivo } = req.body;
+
+    if (!nome || !casa) {
+        return res.status(400).json({
+            success: false,
+            message: "Nome e casa são obrigatórios para um bruxo!"
+        });
+}
+
+ const novoBruxo = {
+        id: bruxos.length + 1,
+        nome,
+        casa: casa,
+        ano: parseInt(ano),
+        varinha: varinha || "Em desenvolvimento",
+        mascote: mascote || "Em desenvolvimento",
+        patrono: patrono || "Em desenvolvimento",
+        especialidade: especialidade || "Em desenvolvimento",
+        vivo: vivo
+    };
+
+    bruxos.push(novoBruxo);
+    
+    res.status(201).json({
+        success: true,
+        message: "Novo bruxo adicionado a Hogwarts!",
+        data: novoBruxo
+    }); 
+});
+
+// Iniciar servidor escutando na porta definida
+app.listen(serverPort, () => {
+    console.log(`🚀 Servidor rodando em http://localhost:${serverPort} 🚀`);
+});
